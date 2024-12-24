07ced8835d07b88c9995b70e16cf57ab844f8609
Sumeet Mourya <go69ciq@mytum.de>
2024-12-24 18:44:40 +0100

    init2

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api'; // Update with your backend URL
+const API_URL = process.env.REACT_APP_API_URL; // Update with your backend URL
 
 export const getProducts = async () => {
   const response = await axios.get(`${API_URL}/products`);
